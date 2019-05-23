caa76fd07b7831e2166732b5223e9715e5569254
Sergio Romero <ing.isc.srl@gmail.com>
2019-05-22 21:10:34 -0500

    login changed

--- raspArd.py
+++ raspArd.py
@@ -6,7 +6,7 @@ import redis
 
 redis = redis.Redis(host='localhost', port=6379)
 server = Flask(__name__)
-arduino = serial.Serial("/dev/ttyACM0", 9600)
+# arduino = serial.Serial("/dev/ttyACM0", 9600)
 t.sleep(1)
 
 spotlightData = {
@@ -45,7 +45,7 @@ def spotlight_route(room):
         	spotlightData['hourStart'] = '{}'.format(dt.now())
         	redis.hmset(room, spotlightData)
         	instruction = "{}_{}".format(room, data["status"])
-        	arduino.write(instruction.encode('utf-8'))
+        	# arduino.write(instruction.encode('utf-8'))
         elif data["status"] == "off":
         	spotlightData['status'] = 'off'
         	spotlightData['hourEnd'] = '{}'.format(dt.now())
@@ -53,17 +53,21 @@ def spotlight_route(room):
         	spotlightData['timeElapsed'] = getTotalTimeSwitchedOn(room)
         	redis.hmset(room, spotlightData)
         	instruction = "{}_{}".format(room, data["status"])
-        	arduino.write(instruction.encode('utf-8'))
+        	# arduino.write(instruction.encode('utf-8'))
         return jsonify(data, redis.hget(room, 'timeElapsed').decode('utf-8'))
 
 @server.route('/login', methods=['POST'])
 def login_user():
 	if(request.method == 'POST'):
 		data = request.get_json()
-		user = redis.hgetall(data['username'])
+		user = redis.hgetall(data['userType'])
 		dataDecoded = {key.decode('utf-8'): value.decode('utf-8') for (key, value) in user.items()}
+		if dataDecoded['userType'] == 'admin':
+			spotlights = ['one', 'two', 'three', 'four']
+		else:
+			spotlights = ['one', 'two']
 		if sha256.verify(data['password'], dataDecoded['password']):
-			return jsonify({'message': 'Hello {}'.format(data['username'])})
+			return jsonify({'message': 'Hello {}'.format(data['userType']), 'spotlights': spotlights})
 		else:
 			return jsonify({'message': 'Wrong access'})
 
@@ -71,11 +75,13 @@ def login_user():
 def add_update():
 	if(request.method == 'POST'):
 		data = request.get_json()
-		for value in ('username', 'password', 'usertype'):
+		if data['userType'] != 'admin' and data['userType'] != 'simple':
+			return jsonify({'message': 'Tipos de usuario invalido'}), 400
+		for value in ('password', 'userType'):
 			userData[value] = data[value] if value!='password' else sha256.hash(data[value])
-		redis.hmset(data['username'], userData)
+		redis.hmset(data['userType'], userData)
 		return jsonify({'message': 'Saved!'})
 
 if __name__ == '__main__':
-    server.run(debug=True, port=5000)
-    arduino.close()
+    server.run(debug=True, port=5001)
+    # arduino.close()
